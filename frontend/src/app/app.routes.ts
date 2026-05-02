2c0d59fdb3486cd34116951bf8bf091430967f70
Shady Elbasiouny <shady.elbasiouny@stud.uni-due.de>
2026-05-03 01:41:03 +0200

    feat(flight):adding flight components and routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import {FlightsComponent} from './pages/flights/flights.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: '**', redirectTo: '' },
+  { path: 'flights', component: FlightsComponent },
+
 ];
